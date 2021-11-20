87d8eb75d2cae750ec33fd6aff6569b82aa949e8
grmontero <grmontero@gmail.com>
2021-11-20 16:16:40 -0500

    Inicial commit prueba

---
+++ CompetenciasContinuasIg/App_Start/StartupAuth.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.IdentityModel.Claims;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Web;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Extensions;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using CompetenciasContinuasIg.Models;
+
+namespace CompetenciasContinuasIg
+{
+    public partial class Startup
+    {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:ClientSecret"];
+        private static string aadInstance = EnsureTrailingSlash(ConfigurationManager.AppSettings["ida:AADInstance"]);
+        private static string authority =  aadInstance + "common"; 
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // This is the resource ID of the AAD Graph API.  We'll need this to request a token to call the Graph API.
+        private static string graphResourceId = "https://graph.windows.net";
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { });
+
+            // instead of using the default validation (validating against a single issuer value, as we do in line of business apps), 
+            // we inject our own multitenant validation logic
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        // If the app needs access to the entire organization, then add the logic
+                        // of validating the Issuer here.
+                        // IssuerValidator
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                         SecurityTokenValidated = (context) =>
+                        {
+                            // If your authentication logic is based on users
+                            return Task.FromResult(0);
+                        },
+                        AuthorizationCodeReceived = (context) =>
+                        {
+                            var code = context.Code;
+
+                            ClientCredential credential = new ClientCredential(clientId, appKey);
+                            string tenantID = context.AuthenticationTicket.Identity.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+                            string signedInUserID = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                            AuthenticationContext authContext = new AuthenticationContext(aadInstance + tenantID, new ADALTokenCache(signedInUserID));
+                            AuthenticationResult result = authContext.AcquireTokenByAuthorizationCodeAsync(
+                                code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, graphResourceId).Result;
+
+                            return Task.FromResult(0);
+                        }
+                    }
+                });
+
+            // This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+            app.UseStageMarker(PipelineStage.Authenticate);
+        }
+
+        private static string EnsureTrailingSlash(string value)
+        {
+            if (value == null)
+            {
+                value = string.Empty;
+            }
+
+            if (!value.EndsWith("/", StringComparison.Ordinal))
+            {
+                return value + "/";
+            }
+
+            return value;
+        }
+    }
+}
---
+++ CompetenciasContinuasIg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CompetenciasContinuasIg.Global" Language="C#" %>
